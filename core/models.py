b9c5886c4b644ec53ba3ed06f2e135cf952c631e
Ali Ganji <aliganji142879@gmail.com>
2024-09-19 14:16:15 +0330

    add permission in model customer

--- core/models.py
+++ core/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(
         unique=True, max_length=254, verbose_name='email address')
+
--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from uuid import uuid4
 
+
 class Category(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500, blank=True)
@@ -19,7 +20,8 @@ class Discount(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name='products')
     slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -30,13 +32,23 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     phone_number = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    class Meta:
+        permissions = [
+            ('send_private_email', 'Can send private email to user bye the button')
+        ]
+
 
 class Address(models.Model):
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, primary_key=True)
     province = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     street = models.CharField(max_length=255)
@@ -47,19 +59,23 @@ class Order(models.Model):
     ORDER_STATUS_UNPAID = 'u'
     ORDER_STATUS_CANCELED = 'c'
     ORDER_STATUS = [
-        (ORDER_STATUS_PAID,'Paid'),
-        (ORDER_STATUS_UNPAID,'Unpaid'),
-        (ORDER_STATUS_CANCELED,'Canceled'),
+        (ORDER_STATUS_PAID, 'Paid'),
+        (ORDER_STATUS_UNPAID, 'Unpaid'),
+        (ORDER_STATUS_CANCELED, 'Canceled'),
     ]
-    
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='orders')
+
+    customer = models.ForeignKey(
+        Customer, on_delete=models.PROTECT, related_name='orders')
     datetime_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+    status = models.CharField(
+        max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items')
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name='order_items')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
@@ -77,11 +93,13 @@ class Comment(models.Model):
         (COMMENT_STATUS_NOT_APPROVED, 'Not Approved'),
     ]
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=255)
     body = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITING)
+    status = models.CharField(
+        max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITING)
 
 
 class Cart(models.Model):
@@ -90,8 +108,10 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='cart_items')
     quantity = models.PositiveSmallIntegerField()
 
     class Meta:
