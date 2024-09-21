08137c07d9557e0f4315f883d76c1b005348f512
Ali Ganji <aliganji142879@gmail.com>
2024-09-21 14:52:07 +0330

    get access token in refresh token

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7)
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME':timedelta(seconds=30)
 }
 
 
