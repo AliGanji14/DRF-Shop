01c6e31f5c5a72a49f0110c48f4f9d041f6c47bd
Ali Ganji <aliganji142879@gmail.com>
2024-09-22 20:13:47 +0330

    add transaction in serializer OrderUpdate

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from decimal import Decimal
 from django.utils.text import slugify
-
+from django.db import transaction
 from .models import Product, Category, Comment, Customer, Cart, CartItem, Order, OrderItem
 
 
@@ -194,28 +194,29 @@ class OrderCreateSerializer(serializers.Serializer):
         return cart_id
 
     def save(self, **kwargs):
-        cart_id = self.validated_data['cart_id']
-        user_id = self.context['user_id']
-        customer = Customer.objects.get(user_id=user_id)
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            user_id = self.context['user_id']
+            customer = Customer.objects.get(user_id=user_id)
 
-        order = Order()
-        order.customer = customer
-        order.save()
+            order = Order()
+            order.customer = customer
+            order.save()
 
-        cart_items = CartItem.objects.select_related(
-            'product').filter(cart_id=cart_id)
+            cart_items = CartItem.objects.select_related(
+                'product').filter(cart_id=cart_id)
 
-        order_items = [
-            OrderItem(
-                order=order,
-                product=cart_item.product,
-                unit_price=cart_item.product.unit_price,
-                quantity=cart_item.quantity,                                                                                                                                                                                                            
-            ) for cart_item in cart_items
-        ]
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=cart_item.product,
+                    unit_price=cart_item.product.unit_price,
+                    quantity=cart_item.quantity,                                                                                                                                                                                                            
+                ) for cart_item in cart_items
+            ]
 
-        OrderItem.objects.bulk_create(order_items)
+            OrderItem.objects.bulk_create(order_items)
 
-        Cart.objects.get(id=cart_id).delete()
+            Cart.objects.get(id=cart_id).delete()
 
-        return order
+            return order
