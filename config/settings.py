07bf7e9d0fba987859179d9a04d284f2d3da99d9
aliganji14 <aliganji142879@gmail.com>
2026-07-07 16:08:58 +0330

    Remove debug toolbar references and update requirements for consistency

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "djoser",
-    "debug_toolbar",
     "store",
     "core",
 ]
@@ -49,16 +48,12 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 ROOT_URLCONF = "config.urls"
 
